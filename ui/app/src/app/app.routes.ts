39105425bf6cdb5532893a649216ae7a630e3559
La-Kiks <kilian.audroin@gmail.com>
2025-06-05 19:53:38 +0200

    Added CV

--- ui/app/src/app/app.routes.ts
+++ ui/app/src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { ProjectOneComponent } from './core/project-one/project-one.component';
 import { ProjectTwoComponent } from './core/project-two/project-two.component';
 import { ProjectThreeComponent } from './core/project-three/project-three.component';
 import { ProjectFourComponent } from './core/project-four/project-four.component';
+import { CurriculumComponent } from './core/curriculum/curriculum.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
+  { path: 'curriculum', component: CurriculumComponent },
   { path: 'project-one', component: ProjectOneComponent },
   { path: 'project-two', component: ProjectTwoComponent },
   { path: 'project-three', component: ProjectThreeComponent },
